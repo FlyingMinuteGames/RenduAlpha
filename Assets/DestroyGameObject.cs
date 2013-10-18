4d32fc9e1f29389dda57c3a72cb803859948528d
Jean-Vincent Lamberti <jv.lamberti@live.fr>
2013-10-18 06:24:44 +0200

    Early Build

---
+++ Assets/DestroyGameObject.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyGameObject : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Destroy(transform.gameObject);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/Scripts/BombScript.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BombScript : MonoBehaviour {
+
+    public delegate void Callback();
+    public Callback callback;
+    private float timer = 4;
+	// Use this for initialization
+	void Start () {
+        
+	}
+
+    /*IEnumerator WaitAndExplode()
+    {
+        Debug.Log("begin explode");
+        yield return new WaitForSeconds(timer);
+        if (callback != null)
+            callback();
+        Debug.Log("explode!");
+    }*/
+
+    void OnNetworkInstantiate(NetworkMessageInfo info)
+    {
+        NetworkMgr.Instance.RegisterObj(this,NetworkMgr.ObjectType.OBJECT_BOMB);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (timer <= 0)
+        {
+            Debug.Log("explode!");
+            if (callback != null)
+                callback();
+        }
+        else timer -= Time.deltaTime;
+	}
+}
